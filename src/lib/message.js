a208cba063f3a6bc66f95a523036246735f8df30
jacktpy <824945044@qq.com>
2022-10-15 16:00:02 +0800

    新增pushplus

--- src/lib/message.js
+++ src/lib/message.js
@@ -1,10 +1,12 @@
 const bot = require('./dingtalk-bot')
 const WXWorkNotify = require('./WXWorkNotify-bot.js')
 const feishuBot = require('./feishu-bot')
+const PushPlus = require('./pushPlus')
 
 module.exports = function message(msg) {
   console.log(msg)
   bot.sendMessage(msg)
   WXWorkNotify(msg)
   feishuBot.sendMessage(msg)
+  PushPlus(msg)
 }
